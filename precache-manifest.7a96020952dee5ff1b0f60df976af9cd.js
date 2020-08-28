f18d10ef6b98e0003a3deb36ee14068262bd914f
vvvtoom <vvvtoom@gmail.com>
2020-08-28 18:33:49 +0900

    Updates

---
+++ precache-manifest.7a96020952dee5ff1b0f60df976af9cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0223cea6a8bdade3b0c0563767948185",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4db5a071292415c87a26",
+    "url": "/movie_app/static/css/main.e608d46b.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db5a071292415c87a26",
+    "url": "/movie_app/static/js/main.89d0ff89.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
