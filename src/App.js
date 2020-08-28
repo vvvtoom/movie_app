b42b4637c6e71b3427aa3faf2250a5f22464e8f7
vvvtoom <vvvtoom@gmail.com>
2020-08-28 18:26:39 +0900

    Making movie app finished

--- src/App.js
+++ src/App.js
@@ -21,20 +21,23 @@ class App extends React.Component {
     return <section className="container">
       {isLoading 
         ? <div className="loader">
-          <span className="loader__text">Loading...</span>
+            <span className="loader__text">Loading...</span>
         </div>
         : (
           <div className="movies">
-            {movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres} />
-            ))}
+            <h1 className="title">ShiRootTeok's 20 Free Film</h1>
+            <div className="movies__items">
+              {movies.map(movie => (
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres} />
+              ))}
+            </div>
           </div>
         )
       }
