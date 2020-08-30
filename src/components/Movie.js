b3fdf60251777e2b4c3f26cb174330dd6ea29269
vvvtoom <vvvtoom@gmail.com>
2020-08-30 22:37:54 +0900

    Add navigation with Router

---
+++ src/components/Movie.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+import "./Movie.css";
+
+function Movie({ year, title, summary, poster, genres }) {
+    return <Link to={{
+        pathname: "/movie-detail",
+        state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+        }
+    }}>
+            <div className="movies__item">
+            <h3 className="movies__item__title">- {title}({year})</h3>
+            <img className="movies__item__image" src={poster} alt={title} title={title} />
+            <div className="movies__item__data">
+                <h3 className="movies__item__genre_title">Genre :</h3>
+                <ul className="movies__item__genres">{genres.map((genre, index) => (
+                    <li key={index} className="movies__item__genres__genre">{genre}</li>     
+                ))}</ul>
+                <p className="movies__item__summary"><b>Summary: </b>{summary.slice(0, 125)}...</p>
+            </div>
+        </div>
+    </Link>
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function About(props) {
+    return <span>About this page: I built it to practice React!</span>
+}
+
+export default About;
